5c19e6f5e2dc253265992b97dac8a3a21632791e
vvvoltalelystad <jaccox10@gmail.com>
2026-07-11 22:50:30 +0200

    Updates

--- music/config.js
+++ music/config.js
@@ -1,5 +1,5 @@
 window.DMQ_CONFIG = {
   SUPABASE_URL: "https://rxhmerqsbxdomfahxjrz.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_Wg-drKeYxE1mufY3XlmkKg_GVM1w0W9",
-  APP_NAME: "Disney Music Quest"
+  APP_NAME: "Mickey's Music Match"
 };
