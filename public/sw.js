ae4da7860d7ce0978e617711e232ef01bf8a8e99
vvvoltalelystad <jaccox10@gmail.com>
2026-07-12 09:27:47 +0200

    fix: refresh app shell cache

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,14 @@
-const CACHE='road-quest-v12';
+const CACHE='road-quest-v13';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png?v=5','./icon-512.png?v=5'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
   self.clients.claim(),
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
 ])));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).catch(()=>caches.match('./')));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
